f480f2d35e347f4748b93c3c1beef7d296421494
Xiuwen Zheng <zhengxwen@gmail.com>
2015-01-14 15:57:56 -0800

    VignetteBuilder: knitr

--- src/R_SeqArray.c
+++ src/R_SeqArray.c
@@ -1,21 +1,10 @@
-// ===========================================================
-//     _/_/_/   _/_/_/  _/_/_/_/    _/_/_/_/  _/_/_/   _/_/_/
-//      _/    _/       _/             _/    _/    _/   _/   _/
-//     _/    _/       _/_/_/_/       _/    _/    _/   _/_/_/
-//    _/    _/       _/             _/    _/    _/   _/
-// _/_/_/   _/_/_/  _/_/_/_/_/     _/     _/_/_/   _/_/
-// ===========================================================
 //
 // R_SeqArray.c: link to R_GDS2.h
-//
-// Copyright (C) 2014-2015    Xiuwen Zheng
-//
-// This file is part of CoreArray.
 //
 
 #include <R_GDS.h>
 
-// do not modify this file, R_GDS2.h is from the gdsfmt package
+// do not modify this file, R_GDS.c/R_GDS2.h is from the gdsfmt package
 
 #if (defined(GDSFMT_R_VERSION) && (GDSFMT_R_VERSION>=0x010103))
 #   include <R_GDS2.h>
