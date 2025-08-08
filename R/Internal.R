ee4338805b6a94e51a2a6127b269c365b1a38563
Xiuwen Zheng <zhengxwen@users.noreply.github.com>
2025-08-08 01:21:16 -0500

    reduce balancing_multiple

--- R/Internal.R
+++ R/Internal.R
@@ -29,7 +29,7 @@ process_count <- 1L
     data_class = "SeqVarDataList"
 )
 
-process_balancing_multiple <- 4L
+process_balancing_multiple <- 2L
 
 
 #######################################################################
